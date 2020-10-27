82ea5d2a2736c5358ba7e4f17535425522b834a9
smitptl <smitvptl@gmail.com>
2020-10-26 17:17:24 -0700

    add firebase

---
+++ build/precache-manifest.ef178aef87284df5e681fcf82f4ca7b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b6d9200eced1ba30c98e319f1a9312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15acaf83f8b771550aa6",
+    "url": "/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "1fdc9c970a4a6911764d",
+    "url": "/static/css/main.d5633e4b.chunk.css"
+  },
+  {
+    "revision": "15acaf83f8b771550aa6",
+    "url": "/static/js/2.f5c2686b.chunk.js"
+  },
+  {
+    "revision": "b2df6f42d3b318f0b6ad3605dbfe5698",
+    "url": "/static/js/2.f5c2686b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdc9c970a4a6911764d",
+    "url": "/static/js/main.7af3942d.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
